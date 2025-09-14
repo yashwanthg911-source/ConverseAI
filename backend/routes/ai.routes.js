3e05adbbe504482020497d9ef9846745d4b38ed3
yashwanthg911-source <yashwanthg911@gmail.com>
2025-09-14 19:16:32 +0530

    Delete backend directory

--- backend/routes/ai.routes.js
+++
@@ -1,8 +0,0 @@
-import { Router } from 'express';
-import * as aiController from '../controllers/ai.controller.js';
-const router = Router();
-
-router.get('/get-result', aiController.getResult)
-
-
-export default router;
